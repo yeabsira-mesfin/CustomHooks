940d177b262387cb467fe0dfaa28a815300cfb4f
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-08-13 06:57:38 -0400

    using custom hooks for availablePlaces functionalities

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,15 +1,34 @@
-import { useState, useEffect } from 'react';
 import { useFetch } from '../hooks/useFetch.js';
 import Places from './Places.jsx';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
 
+async function fetchSortedPlaces(){
+  const places = await fetchAvailablePlaces();
+
+  return new Promise((resolve)=>{
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      resolve(sortedPlaces);
+    });
+  })
+
+
+ 
+
+
+}
+
 export default function AvailablePlaces({ onSelectPlace }) {
 
   const {isFetching,
-    error,fetchedData:availablePlaces,
-    setFetchedData:setAvailablePlaces} = useFetch(fetchAvailablePlaces,[]);
+    error,fetchedData:availablePlaces} = useFetch(fetchSortedPlaces,[]);
 
 
   if (error) {
